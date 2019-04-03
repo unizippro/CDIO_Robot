eb2a51811e2b36ffff05f29e4cd754bb79f8b292
Magnus Hansen <magnus.h1993@gmail.com>
2019-04-03 09:11:23 +0200

    Temporary fix ip for robot

--- robot/src/main/java/Program.java
+++ robot/src/main/java/Program.java
@@ -1,6 +1,5 @@
 import robot.Robot;
 
-import java.net.InetAddress;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
@@ -14,15 +13,13 @@ public class Program {
     }
 
     private Program() throws Exception {
-        String ipAddress = InetAddress.getLocalHost().getHostAddress();
-
         Robot robot = new Robot();
 
         try {
             LocateRegistry.createRegistry(1199);
-            Naming.rebind("rmi://" + ipAddress + ":1199/robot", robot);
-            Naming.rebind("rmi://" + ipAddress + ":1199/movement", robot.getMovement());
-            Naming.rebind("rmi://" + ipAddress + ":1199/sensors", robot.getSensors());
+            Naming.rebind("rmi://" + "192.168.2.6" + ":1199/robot", robot);
+            Naming.rebind("rmi://" + "192.168.2.6" + ":1199/movement", robot.getMovement());
+            Naming.rebind("rmi://" + "192.168.2.6" + ":1199/sensors", robot.getSensors());
         } catch (RemoteException exception) {
             exception.printStackTrace();
             System.exit(1);
