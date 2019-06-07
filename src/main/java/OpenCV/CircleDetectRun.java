d7b2e3b46ff1b4791486aebef8a07cc7b020c404
Magnus Hansen <magnus.h1993@gmail.com>
2019-06-07 23:44:12 +0200

    Restructure BallDetector

--- src/main/java/OpenCV/CircleDetectRun.java
+++ src/main/java/OpenCV/CircleDetectRun.java
@@ -1,12 +1,40 @@
 package OpenCV;
 
+import group14.Resources;
 import group14.opencv.detectors.BallDetector;
 import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.highgui.HighGui;
+import org.opencv.imgcodecs.Imgcodecs;
 
 public class CircleDetectRun {
     public static void main(String[] args) {
         // Load the native library.
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        new BallDetector().run(args);
+
+//        VideoCapture cap = new VideoCapture(0);
+//        if (! cap.isOpened()) {
+//            System.out.println("Error");
+//            System.exit(1);
+//        }
+//
+//        Mat src = new Mat();
+//        cap.read(src);
+
+        Mat src = Imgcodecs.imread(Resources.TestImages.board4, Imgcodecs.IMREAD_COLOR);
+
+        var ballDetector = new BallDetector(src);
+
+        ballDetector.run();
+
+        var result = ballDetector.getResult();
+
+        result.getValue().forEach(System.out::println);
+
+        HighGui.imshow("Result", result.getKey());
+        HighGui.resizeWindow("Result", 680, 400);
+        HighGui.waitKey();
+
+        System.exit(0);
     }
 }
--- src/main/java/group14/opencv/detectors/BallDetector.java
+++ src/main/java/group14/opencv/detectors/BallDetector.java
@@ -1,32 +1,33 @@
 package group14.opencv.detectors;
 
+import javafx.util.Pair;
 import org.opencv.core.*;
 import org.opencv.core.Point;
-import org.opencv.highgui.HighGui;
-import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
-import org.opencv.videoio.VideoCapture;
 
-import static org.opencv.imgcodecs.Imgcodecs.imread;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class BallDetector {
 
-    public void run(String[] args) {
-        Mat src = new Mat();
-        VideoCapture cap = new VideoCapture(1);
-        if(!cap.isOpened()) {
-            System.out.println("Error");
-        }
-        else {
+    private ArrayList<Point> points = new ArrayList<>();
+    private Mat src;
+    private Mat out;
 
-            cap.read(src);
-        }
 
+    public BallDetector(Mat src) {
+        this.src = src;
+        this.out = src;
+    }
+
+
+    public void run() {
         //Blur mat to better define objects
-        Imgproc.medianBlur(src, src, 5);
+        var blurred = this.blur(this.src, 5);
+
         //Set threshold
-        Mat thresh = new Mat();
-        Core.inRange(src, new Scalar(185, 185, 185), new Scalar(255, 255, 255), thresh);
+        var thresh = this.threshold(blurred, 185, 255);
 
         //! [houghcircles]
         Mat circles = new Mat();
@@ -36,26 +37,36 @@ public class BallDetector {
         for (int x = 0; x < circles.cols(); x++) {
             double[] c = circles.get(0, x);
             Point center = new Point(Math.round(c[0]), Math.round(c[1]));
+            this.points.add(center);
+
             // circle center
-            Imgproc.circle(src, center, 1, new Scalar(0,100,100), 3, 8, 0 );
+            Imgproc.circle(this.out, center, 1, new Scalar(0, 100, 100), 3, 8, 0);
+
             // circle outline
             int radius = (int) Math.round(c[2]);
-            Imgproc.circle(src, center, radius, new Scalar(255,0,255), 3, 8, 0 );
-
-            System.out.println(center);
+            Imgproc.circle(this.out, center, radius, new Scalar(255, 0, 255), 3, 8, 0);
         }
         //! [draw]
+    }
 
-        //rezise img
-        Mat reziseImg = new Mat();
-        Size scaleSize = new Size(1000,680);
-        Imgproc.resize(src, reziseImg, scaleSize, 0, 0, Imgproc.INTER_AREA);
 
-        //! [display]
-        HighGui.imshow("detected circles", reziseImg);
-        HighGui.waitKey();
-        //! [display]
+    public Pair<Mat, List<Point>> getResult() {
+        return new Pair<>(this.out, this.points);
+    }
 
-        System.exit(0);
+
+    private Mat blur(Mat src, int ksize) {
+        Mat out = new Mat();
+        Imgproc.medianBlur(src, out, ksize);
+
+        return out;
+    }
+
+
+    private Mat threshold(Mat src, double lower, double upper) {
+        Mat out = new Mat();
+        Core.inRange(src, new Scalar(lower, lower, lower), new Scalar(upper, upper, upper), out);
+
+        return out;
     }
 }
