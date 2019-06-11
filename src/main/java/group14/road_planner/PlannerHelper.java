5c95491cd6ec2d322800ca24f1b7ad871d9a0df6
Oliver <oliverskoeppen@gmail.com>
2019-06-11 15:58:57 +0200

    fixed rotational point
    - changed to +

--- src/main/java/group14/road_planner/PlannerHelper.java
+++ src/main/java/group14/road_planner/PlannerHelper.java
@@ -11,6 +11,6 @@ public class PlannerHelper {
     public double getAngle(Robot robot, Point ballPoint) {
         double robotRot = Math.toDegrees(Math.atan2(robot.getFront().getY() - robot.getRearOpenCVPoint().getY(), robot.getFront().getX() - robot.getRearOpenCVPoint().getX()));
         double ballRot = Math.toDegrees(Math.atan2(ballPoint.getY() - robot.getRotationalPoint().getY(), ballPoint.getX() - robot.getRotationalPoint().getX()));
-        return robotRot-ballRot;
+        return robotRot+ballRot;
     }
 }
