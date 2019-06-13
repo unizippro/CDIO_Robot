d7a5ba2862888b546abe1b425ef81a9183e15cdb
AM <AM>
2019-06-13 14:51:27 +0200

    Added guard for nullpointerexception if robotpoints (front back) is null

--- src/main/java/group14/opencv/detectors/robot_detector/RobotDetectorResult.java
+++ src/main/java/group14/opencv/detectors/robot_detector/RobotDetectorResult.java
@@ -9,36 +9,28 @@ public class RobotDetectorResult {
     private Mat output;
     private Mat outputThreshBlue;
     private Mat outputThreshGreen;
-    private List<Point> pointsFront;
-    private List<Point> pointsBack;
-    private List<Point> robotPoints;
 
-    RobotDetectorResult(Mat output, Mat outputThreshBlue, Mat outputThreshGreen, List<Point> pointsFront, List<Point> pointsBack) {
+    private Point pointFront;
+    private Point pointBack;
+
+    RobotDetectorResult(Mat output, Mat outputThreshBlue, Mat outputThreshGreen, Point pointFront, Point pointBack) {
         this.output = output;
         this.outputThreshBlue = outputThreshBlue;
         this.outputThreshGreen = outputThreshGreen;
-        this.pointsFront = pointsFront;
-        this.pointsBack = pointsBack;
+        this.pointFront = pointFront;
+        this.pointBack = pointBack;
     }
 
     public Mat getOutput() {
         return this.output;
     }
 
-    public List<Point> getPointsFront() {
-        return this.pointsFront;
+    public Point getPointFront() {
+        return this.pointFront;
     }
 
-    public List<Point> getPointsBack() {
-        return this.pointsBack;
-    }
-
-    public List<Point> getPoints() {
-        
-        robotPoints.add(pointsFront.get(0));
-        robotPoints.add(pointsBack.get(0));
-
-        return this.robotPoints;
+    public Point getPointBack() {
+        return this.pointBack;
     }
 
     public Mat getOutputThreshBlue() {
