bf1686e117c996b2cf36e09204c80e53b3f67e65
Magnus Hansen <magnus.h1993@gmail.com>
2019-06-14 23:18:29 +0200

    Add test calibration images

--- src/main/java/group14/Resources.java
+++ src/main/java/group14/Resources.java
@@ -1,9 +1,9 @@
 package group14;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -46,6 +46,36 @@ public class Resources {
     }
 
 
+    public static class OpenCVSamples {
+        public static final String left1 = load("left01.jpg");
+        public static final String left2 = load("left02.jpg");
+        public static final String left3 = load("left03.jpg");
+        public static final String left4 = load("left04.jpg");
+        public static final String left5 = load("left05.jpg");
+        public static final String left6 = load("left06.jpg");
+        public static final String left7 = load("left07.jpg");
+        public static final String left8 = load("left08.jpg");
+        public static final String left9 = load("left09.jpg");
+        public static final String left11 = load("left11.jpg");
+        public static final String left12 = load("left12.jpg");
+        public static final String left13 = load("left13.jpg");
+        public static final String left14 = load("left14.jpg");
+
+
+        public static List<String> getAllFiles() {
+            return Resources.getAllFiles("opencv_samples", false);
+        }
+
+        public static List<String> getAllChessboardFiles() {
+            return Resources.getAllFilesMatching("**/left*.jpg", "opencv_samples");
+        }
+
+        private static String load(String file) {
+            return Resources.load("opencv_samples/" + file);
+        }
+    }
+
+
     private static String load(String file) {
         var fileUrl = ClassLoader.getSystemResource("group14/" + file);
 
@@ -65,6 +95,39 @@ public class Resources {
                     .map(path -> onlyFileName ? path.getFileName() : path)
                     .map(Path::toString)
                     .collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+
+    private static List<String> getAllFilesMatching(String glob, String dir) {
+        var filePath = ClassLoader.getSystemResource("group14/" + dir).getFile();
+
+        final PathMatcher pathMatcher = FileSystems.getDefault().getPathMatcher("glob:" + glob);
+
+        try {
+            var matchedPaths = new ArrayList<String>();
+
+            Files.walkFileTree(Paths.get(filePath), new SimpleFileVisitor<>() {
+                @Override
+                public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) {
+                    if (pathMatcher.matches(path)) {
+                        matchedPaths.add(path.toString());
+                    }
+
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFileFailed(Path file, IOException exc) {
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+
+            return matchedPaths;
         } catch (IOException e) {
             e.printStackTrace();
         }
