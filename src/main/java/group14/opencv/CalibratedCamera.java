a423c1894f87f0b91430cb02a5377114259d0574
Unizippro <e>
2019-06-17 13:41:19 +0200

    Homografy Implementation place

--- src/main/java/group14/opencv/CalibratedCamera.java
+++ src/main/java/group14/opencv/CalibratedCamera.java
@@ -85,6 +85,7 @@ public class CalibratedCamera extends Camera {
 
             if (this.isCalibrated) {
                 Calib3d.undistort(frame, outFrame, this.intrinsic, this.distCoeffs);
+                //Homeography here.
             } else {
                 this.drawCalibration(frame, outFrame);
             }
